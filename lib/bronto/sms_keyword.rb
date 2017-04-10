7c9bb8ded5f9a03d9b11f638fa9547e621f94b12
Nathan <querky1231@gmail.com>
2017-04-10 14:03:45 -0400

    Add class for SMS Keyword

---
+++ lib/bronto/sms_keyword.rb
@@ -0,0 +1,64 @@
+module Bronto
+  class SMSKeyword < Base
+    attr_accessor :name, :description, :subscriber_count, :frequency_cap, :date_created,
+                  :scheduled_delete_date, :confirmation_message, :message_content, :keyword_type
+
+    def self.plural_class_name
+      'sms_keywords'
+    end
+
+    def initialize(options = {})
+      super(options)
+      self.subscriber_count ||= 0
+    end
+
+    def add_to_keyword(*contacts)
+      begin
+        add_to_keyword!(contacts)
+      rescue Bronto::Error => e
+        false
+      end
+    end
+
+    # Adds the given contacts to this SMS Keyword.
+    def add_to_keyword!(*contacts)
+      return false if !self.id.present?
+      contacts = contacts.flatten
+
+      # The block below is evaluated in a weird scope so we need to capture self as _self for use inside the block.
+      _self = self
+
+      resp = request("add_to_sms_keyword", {keyword: { id: _self.id }, contacts: contacts.map { |c| { id: c.id } }})
+
+      errors = Array.wrap(resp[:return][:results]).select { |r| r[:is_error] }
+      errors.each do |error|
+        raise Bronto::Error.new(error[:error_code], error[:error_string])
+      end
+
+      true
+    end
+
+    # Removes the given contacts from this SMS Keyword.
+    def remove_from_keyword(*contacts)
+      return false if !self.id.present?
+      contacts = contacts.flatten
+
+      resp = request("remove_from_sms_keyword", {keyword: self.to_hash, contacts: contacts.map(&:to_hash)})
+
+      Array.wrap(resp[:return][:results]).select { |r| r[:is_error] }.count == 0
+    end
+
+    def subscriber_count=(new_val)
+      @subscriber_count = new_val.to_i
+    end
+
+
+    def to_hash
+      hash = { name: name, description: description, subscriber_count: subscriber_count, frequency_cap: frequency_cap,
+               date_created: date_created, scheduled_delete_date: scheduled_delete_date, confirmation_message: confirmation_message,
+               message_content: message_content, keyword_type: keyword_type }
+      hash[:id] = id if id.present?
+      hash
+    end
+  end
+end
